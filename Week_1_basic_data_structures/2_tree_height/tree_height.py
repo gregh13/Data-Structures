c447c2811c95024296ff698e664f2f39e518ebeb
Greg <gregh13@protonmail.com>
2022-09-30 17:21:34 +0700

    add comments, clean up code

--- Week_1_basic_data_structures/2_tree_height/tree_height.py
+++ Week_1_basic_data_structures/2_tree_height/tree_height.py
@@ -1,7 +1,5 @@
-# python3
-
-import sys
-import threading
+# import sys
+# import threading
 
 
 class Node:
@@ -15,32 +13,50 @@ class Node:
 
 def build_tree(n, parents):
 
+    # Initialize root value
     root = None
+
+    # Initialize node array with blank, unique Nodes
     node_array = [Node() for x in range(n)]
 
+    # Assignment input has a list of values (parents). The index value in the list is their own index (the child_index)
     for child_index in range(n):
+
+        # The value that is stored at the child_index is their parent's index in the list
         parent_index = parents[child_index]
+
+        # Per assignment stipulations, input list contains exactly one root, marked by a value of -1
         if parent_index == -1:
             root = child_index
         else:
+            # Update parent node's children with the child node object
             node_array[parent_index].add_child(node_array[child_index])
 
+    # Return root node
     return node_array[root]
 
 
 def find_height(root):
     if root is None:
         return 0
+
+    # Initialize root level and set as node
     node = root
     node.level = 1
+
+    # Initialize queue, enqueue root node
     queue_array = [node]
 
     while queue_array:
+        # Dequeue
         node = queue_array.pop(0)
+
+        # Update each child node's level
         for child in node.children:
             child.level = node.level + 1
             queue_array.append(child)
 
+    # Either returns most recent node level
     return node.level
 
 
