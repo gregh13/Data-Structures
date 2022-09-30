2bb299f698f1df794198350f18f95b3058bac823
Greg <gregh13@protonmail.com>
2022-09-30 16:17:52 +0700

    calculate height based on number of children, fix bugs (range issue), comment out starter recursion depth change/threading

--- Week_1_basic_data_structures/2_tree_height/tree_height.py
+++ Week_1_basic_data_structures/2_tree_height/tree_height.py
@@ -14,27 +14,35 @@ class Node:
 
 def build_tree(n, parents):
 
-    node_array = [Node] * n
+    root = None
+    node_array = [Node()] * n
 
-    for child_index in range(len(n)):
+    for child_index in range(n):
         parent_index = parents[child_index]
         if parent_index == -1:
             root = child_index
         else:
             node_array[parent_index].add_child(node_array[child_index])
 
-    find_height(root)
+    return node_array[root]
 
 
 def find_height(root):
-    if not root:
+    if root is None:
         return 0
 
     queue_array = [root]
     height = 0
     while queue_array:
         node = queue_array.pop(0)
-        queue_array.append(*node.children)
+        height += 1
+        children = node.children
+        if children:
+            height -= (len(children) - 1)
+            queue_array.append(*children)
+
+    return height
+
 
 def compute_height(n, parents):
     # Replace this code with a faster implementation
@@ -53,15 +61,16 @@ def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
-    build_tree(n, parents)
+    root = build_tree(n, parents)
+    print(find_height(root))
 
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+# sys.setrecursionlimit(10**7)  # max depth of recursion
+# threading.stack_size(2**27)   # new thread will get stack of such size
+# threading.Thread(target=main).start()
 
 if __name__ == "__main__":
     main()
