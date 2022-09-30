cf80d558a0d6fabe580b3d764665a8f1b1bc1372
Greg <gregh13@protonmail.com>
2022-09-30 15:59:16 +0700

    create Node class and node array, implement build_tree function and start on find_height function

--- Week_1_basic_data_structures/2_tree_height/tree_height.py
+++ Week_1_basic_data_structures/2_tree_height/tree_height.py
@@ -4,6 +4,38 @@ import sys
 import threading
 
 
+class Node:
+    def __init__(self):
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
+def build_tree(n, parents):
+
+    node_array = [Node] * n
+
+    for child_index in range(len(n)):
+        parent_index = parents[child_index]
+        if parent_index == -1:
+            root = child_index
+        else:
+            node_array[parent_index].add_child(node_array[child_index])
+
+    find_height(root)
+
+
+def find_height(root):
+    if not root:
+        return 0
+
+    queue_array = [root]
+    height = 0
+    while queue_array:
+        node = queue_array.pop(0)
+        queue_array.append(*node.children)
+
 def compute_height(n, parents):
     # Replace this code with a faster implementation
     max_height = 0
@@ -21,6 +53,7 @@ def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
+    build_tree(n, parents)
 
 
 # In Python, the default limit on recursion depth is rather low,
@@ -29,3 +62,6 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
+
+if __name__ == "__main__":
+    main()
