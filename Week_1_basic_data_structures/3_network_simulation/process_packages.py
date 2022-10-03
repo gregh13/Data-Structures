18a2ab698c36d0a168988aed127bf64187a2b325
Greg <gregh13@protonmail.com>
2022-10-03 10:19:16 +0700

    change deque to simple queue list since deque functionality isn't needed.

--- Week_1_basic_data_structures/3_network_simulation/process_packages.py
+++ Week_1_basic_data_structures/3_network_simulation/process_packages.py
@@ -1,6 +1,6 @@
 # python3
 
-from collections import namedtuple, deque
+from collections import namedtuple
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
@@ -10,37 +10,37 @@ class Buffer:
     def __init__(self, size):
         self.size = size
         self.finish_time = []
-        self.deque = deque([])
+        self.queue = []
 
     def process(self, request):
-        # Check for any already processed packets, remove from deque
-        if self.deque:
-            next_up = self.deque[0]
+        # Check for any already processed packets, remove from queue
+        if self.queue:
+            next_up = self.queue[0]
             while next_up <= request.arrived_at:
-                self.deque.popleft()
-                if self.deque:
-                    next_up = self.deque[0]
+                self.queue.pop(0)
+                if self.queue:
+                    next_up = self.queue[0]
                 else:
                     next_up = float("inf")
 
-        # Check if buffer is empty after removing any previous; if yes, process packet immediately, add to deque
-        if len(self.deque) == 0:
+        # Check if buffer is empty after removing any previous; if yes, process packet immediately, add to queue
+        if len(self.queue) == 0:
             finishing_time = request.arrived_at + request.time_to_process
             self.finish_time.append(finishing_time)
-            self.deque.append(finishing_time)
+            self.queue.append(finishing_time)
             return Response(False, request.arrived_at)
 
         # Check if buffer is full; if yes, packet gets dropped
-        if len(self.deque) == self.size:
+        if len(self.queue) == self.size:
             return Response(True, -1)
 
-        # Add new packet to end of deque
-        end_of_deque = self.deque[-1]
-        new_deque_end = end_of_deque + request.time_to_process
-        self.deque.append(new_deque_end)
-        self.finish_time.append(new_deque_end)
+        # Add new packet to end of queue
+        end_of_queue = self.queue[-1]
+        new_queue_end = end_of_queue + request.time_to_process
+        self.queue.append(new_queue_end)
+        self.finish_time.append(new_queue_end)
 
-        return Response(False, end_of_deque)
+        return Response(False, end_of_queue)
 
 
 def process_requests(requests, buffer):
