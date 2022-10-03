43d73b35afa00174d19158d914406967c58d95b4
Greg <gregh13@protonmail.com>
2022-10-03 12:15:45 +0700

    use dequeue to store local max and remove smaller items, reducing time spent on finding max()

--- Week_1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ Week_1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,6 +1,22 @@
 from collections import deque
+
+
 def deque_solution(sequence, m):
-    pass
+    max_values = []
+    de = deque([])
+    # Initialize deque with first window elements (excluding last element of window)
+    de.extend(sequence[:m-1])
+
+    for i in range(len(sequence) - m + 1):
+        for _ in range(len(de)):
+            if de[-1] <= sequence[i]:
+                de.pop()
+        de.append(sequence[i])
+        max_values.append(max(de))
+
+
+
+
 
 def max_sliding_window_naive(sequence, m):
     maximums = []
