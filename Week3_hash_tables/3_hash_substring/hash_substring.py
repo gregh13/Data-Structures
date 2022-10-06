777be0e6d8e5e257632de11d1363ca5c16d633a5
Greg <gregh13@protonmail.com>
2022-10-06 22:03:50 +0700

    clean up starter code

--- Week3_hash_tables/3_hash_substring/hash_substring.py
+++ Week3_hash_tables/3_hash_substring/hash_substring.py
@@ -1,18 +1,19 @@
-# python3
-
 def read_input():
-    return (input().rstrip(), input().rstrip())
+    return input().rstrip(), input().rstrip()
+
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
+
+def get_occurrences_naive(pattern, text):
     return [
         i 
         for i in range(len(text) - len(pattern) + 1) 
         if text[i:i + len(pattern)] == pattern
     ]
 
+
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    print_occurrences(get_occurrences_naive(*read_input()))
 
