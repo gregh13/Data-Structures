985040dc1931f083a8df28f930d5ce49febd4fb0
Greg <gregh13@protonmail.com>
2022-10-07 09:39:40 +0700

    add comments, increase prime number check size

--- Week3_hash_tables/3_hash_substring/hash_substring.py
+++ Week3_hash_tables/3_hash_substring/hash_substring.py
@@ -1,6 +1,8 @@
 import random
 
+
 def hash_function(string, multiplier, prime):
+    # Polynomial hash function - loops through each char in string, converts to int, updates hash val
     ans = 0
     for c in reversed(string):
         ans = (ans * multiplier + ord(c)) % prime
@@ -8,26 +10,32 @@ def hash_function(string, multiplier, prime):
 
 
 def hashing_solution(pattern, text):
+    # Store common operations as variables for convenience
     text_len = len(text)
     pattern_len = len(pattern)
     t_minus_p = text_len - pattern_len
 
+    # Initialize hash array and empty match list
     hashes = [None] * (t_minus_p + 1)
     match_positions = []
 
+    # Choosing an appropriately large prime number is essential to keeping hash collisions low
+    # However, too large a prime for a simple input will cause the arithmetic to be slower
     prime_list = [2_207, 3_010_349, 1_000_000_007, 1_000_000_000_193,
                   2_305_843_009_213_693_951, 618_970_019_642_690_137_449_562_111]
-    prime = prime_list[0]
 
+    # Initialize prime number to smallest in list, set counter to next index
+    prime = prime_list[0]
     counter = 1
-    while text_len * pattern_len > prime and counter < len(prime_list):
+
+    while 5 * text_len * pattern_len > prime and counter < len(prime_list):
         prime = prime_list[counter]
         counter += 1
 
     multiplier = random.randint(1, prime - 1)
 
     pattern_hash = hash_function(pattern, multiplier, prime)
-    hashes[(t_minus_p)] = hash_function(text[t_minus_p:], multiplier, prime)
+    hashes[t_minus_p] = hash_function(text[t_minus_p:], multiplier, prime)
 
     y = 1
     for _ in range(pattern_len):
