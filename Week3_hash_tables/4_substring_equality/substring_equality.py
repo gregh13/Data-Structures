33f13f9bcea9be38bbea6cfed89873b2e4896cda
Greg <gregh13@protonmail.com>
2022-10-07 10:14:57 +0700

    store and initialize input attributes

--- Week3_hash_tables/4_substring_equality/substring_equality.py
+++ Week3_hash_tables/4_substring_equality/substring_equality.py
@@ -4,12 +4,26 @@ import sys
 class HashSolution:
 	def __init__(self, string):
 		self.string = string
+		self.s_len = len(string)
+		self.hash_table_1 = [0] * self.s_len
+		self.hash_table_2 = [0] * self.s_len
+		self.mod_1 = (10**9) + 7
+		self.mod_2 = (10**9) + 9
+
+	def query_input(self, a, b, length):
+		self.a = a
+		self.b = b
+		self.l = length
 
 	def hash_func(self, substring, x, prime):
 		hash = 0
 		for char in reversed(substring):
 			hash = (hash * x + ord(char)) % prime
 		return hash
+
+	def precompute_hashes(self, ):
+		for i in range(1, self.s_len):
+
 
 
 
