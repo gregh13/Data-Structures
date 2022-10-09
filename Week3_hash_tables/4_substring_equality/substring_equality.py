3000c40d9ca42af4c9ae8b8e3c3d64aee95c6ace
Greg <gregh13@protonmail.com>
2022-10-09 17:47:05 +0700

    replace single coef variable with two coef_tables (one for each prime). Add coef calculations to precompute function, store coef values for all possible sublength values for constant time access during calc_substring_hash calls.

--- Week3_hash_tables/4_substring_equality/substring_equality.py
+++ Week3_hash_tables/4_substring_equality/substring_equality.py
@@ -10,44 +10,44 @@ class HashSolution:
 		self.hash_table_1 = [0] * self.string_len
 		self.hash_table_2 = [0] * self.string_len
 
+		self.coef_table_1 = [1] * self.string_len
+		self.coef_table_2 = [1] * self.string_len
+
 		self.power = 10 ** 9
 		self.mod_1 = self.power + 7
 		self.mod_2 = self.power + 9
 		self.x = random.randint(1, self.power - 1)
 
-		self.coef = -1
 		self.a_index = -1
 		self.b_index = -1
 		self.sub_len = -1
 
-		self.precompute_hashes()
+		self.precompute_hashes_coefs()
 
-	def precompute_hashes(self):
+	def precompute_hashes_coefs(self):
 		for i in range(1, self.string_len):
 			self.hash_table_1[i] = (self.x * self.hash_table_1[i-1] + ord(self.string[i-1])) % self.mod_1
 			self.hash_table_2[i] = (self.x * self.hash_table_2[i-1] + ord(self.string[i-1])) % self.mod_2
+
+			self.coef_table_1[i] = (self.coef_table_1[i-1] * self.x) % self.mod_1
+			self.coef_table_2[i] = (self.coef_table_2[i-1] * self.x) % self.mod_2
 
 	def query_input(self, a_i, b_i, length):
 		self.a_index = a_i
 		self.b_index = b_i
 		self.sub_len = length
 
-	def calc_substring_hash(self, hash_table, prime):
-		self.coef = self.x
-
-		for i in range(self.sub_len-1):
-			self.coef = (self.coef * self.x) % prime
-
-		a_hash = hash_table[self.a_index + self.sub_len] - (self.coef * hash_table[self.a_index])
-		b_hash = hash_table[self.b_index + self.sub_len] - (self.coef * hash_table[self.b_index])
+	def calc_substring_hash(self, hash_table, prime, coef_table):
+		a_hash = hash_table[self.a_index + self.sub_len] - (coef_table[self.sub_len] * hash_table[self.a_index])
+		b_hash = hash_table[self.b_index + self.sub_len] - (coef_table[self.sub_len] * hash_table[self.b_index])
 
 		a_hash = (a_hash + prime) % prime
 		b_hash = (b_hash + prime) % prime
 		return a_hash == b_hash
 
 	def check_substrings(self):
-		if self.calc_substring_hash(self.hash_table_1, self.mod_1):
-			if self.calc_substring_hash(self.hash_table_2, self.mod_2):
+		if self.calc_substring_hash(self.hash_table_1, self.mod_1, self.coef_table_1):
+			if self.calc_substring_hash(self.hash_table_2, self.mod_2, self.coef_table_2):
 				print("Yes")
 			else:
 				print("No")
