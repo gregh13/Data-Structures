e4a27c34ddc66da113e995038e5e04c86d5373b7
Greg <gregh13@protonmail.com>
2022-10-09 21:51:39 +0700

    double up hash and coef tables and computation. Need two hash functions for each to avoid collisions

--- Week3_hash_tables/5_longest_common_substring/common_substring.py
+++ Week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -9,25 +9,49 @@ Answer = namedtuple('answer_type', 'i j len')
 
 def hashing_algorithm(str_1, str_2):
 	def precompute_hashes_coefs():
-		h_table_1[0] = ord(str_1) % mod_1
-		h_table_2[0] = ord(str_2) % mod_2
+		h_table_1a[0] = ord(str_1) % mod_1
+		h_table_1b[0] = ord(str_1) % mod_2
+
+		h_table_2a[0] = ord(str_2) % mod_1
+		h_table_2b[0] = ord(str_2) % mod_2
 
 		for i in range(1, len_1):
-			h_table_1[i] = (x * h_table_1[i - 1] + ord(str_1[i - 1])) % mod_1
-			coef_table_1[i] = (coef_table_1[i - 1] * x) % mod_1
+			h_table_1a[i] = (x * h_table_1a[i - 1] + ord(str_1[i - 1])) % mod_1
+			coef_table_1a[i] = (coef_table_1a[i - 1] * x) % mod_1
+			h_table_1b[i] = (x * h_table_1b[i - 1] + ord(str_1[i - 1])) % mod_2
+			coef_table_1b[i] = (coef_table_1b[i - 1] * x) % mod_2
 
 		for i in range(1, len_2):
-			h_table_2[i] = (x * h_table_2[i - 1] + ord(str_2[i - 1])) % mod_2
-			coef_table_2[i] = (coef_table_2[i - 1] * x) % mod_2
+			h_table_2a[i] = (x * h_table_2a[i - 1] + ord(str_2[i - 1])) % mod_1
+			coef_table_2a[i] = (coef_table_2a[i - 1] * x) % mod_1
+			h_table_2b[i] = (x * h_table_2b[i - 1] + ord(str_2[i - 1])) % mod_2
+			coef_table_2b[i] = (coef_table_2b[i - 1] * x) % mod_2
+
+	def binary_hash_search():
+		not_found = True
+		k = max_len // 2
+
+		while not_found:
+			for
+				a_hash = hash_table[j + k] - (coef_table[k] * hash_table[a_index])
+				a_hash = (a_hash + prime) % prime
+
+		return a_hash
 
 	len_1 = len(str_1)
 	len_2 = len(str_2)
 
-	h_table_1 = [0] * (len_1)
-	h_table_2 = [0] * (len_2)
+	h_table_1a = [0] * (len_1)
+	h_table_1b = [0] * (len_1)
 
-	coef_table_1 = [1] * (len_1)
-	coef_table_2 = [1] * (len_2)
+	h_table_2a = [0] * (len_2)
+	h_table_2b = [0] * (len_2)
+
+	coef_table_1a = [1] * (len_1)
+	coef_table_1b = [1] * (len_1)
+
+	coef_table_2a = [1] * (len_2)
+	coef_table_2b = [1] * (len_2)
 
 	power = 10 ** 9
 	mod_1 = power + 87
