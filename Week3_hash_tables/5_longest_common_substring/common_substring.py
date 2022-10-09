11144d70941ed3c9e1e907f1eb8ba9b7387da95e
Greg <gregh13@protonmail.com>
2022-10-09 21:39:14 +0700

    begin implementing hashing solution. Initialize variables (hash tables, coefficient tables, modulo primes) and add precomputing function (for hash values and coefs)

--- Week3_hash_tables/5_longest_common_substring/common_substring.py
+++ Week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -2,8 +2,56 @@
 
 import sys
 from collections import namedtuple
+import random
 
 Answer = namedtuple('answer_type', 'i j len')
+
+
+def hashing_algorithm(str_1, str_2):
+	def precompute_hashes_coefs():
+		h_table_1[0] = ord(str_1) % mod_1
+		h_table_2[0] = ord(str_2) % mod_2
+
+		for i in range(1, len_1):
+			h_table_1[i] = (x * h_table_1[i - 1] + ord(str_1[i - 1])) % mod_1
+			coef_table_1[i] = (coef_table_1[i - 1] * x) % mod_1
+
+		for i in range(1, len_2):
+			h_table_2[i] = (x * h_table_2[i - 1] + ord(str_2[i - 1])) % mod_2
+			coef_table_2[i] = (coef_table_2[i - 1] * x) % mod_2
+
+	len_1 = len(str_1)
+	len_2 = len(str_2)
+
+	h_table_1 = [0] * (len_1)
+	h_table_2 = [0] * (len_2)
+
+	coef_table_1 = [1] * (len_1)
+	coef_table_2 = [1] * (len_2)
+
+	power = 10 ** 9
+	mod_1 = power + 87
+	mod_2 = power + 223
+	x = random.randint(1, power)
+
+	max_len = 0
+
+	if len_1 < len_2:
+		max_len = len_1
+	else:
+		max_len = len_2
+
+	precompute_hashes_coefs()
+
+
+
+
+
+
+
+
+
+
 
 
 def solve(s, t):
