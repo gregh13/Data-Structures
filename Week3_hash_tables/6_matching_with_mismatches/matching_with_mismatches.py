0df2d528ceb6312e7ffe8cfd66570880521fc7d0
Greg <gregh13@protonmail.com>
2022-10-10 16:34:54 +0700

    add second set of hash and coef tables (both in init and precompute) for pattern value

--- Week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
+++ Week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
@@ -7,28 +7,48 @@ class HashMismatch:
 		self.k = k
 		self.text = text
 		self.pattern = pattern
+		self.results = []
 
 		self.len_t = len(text) + 1
 		self.len_p = pattern
 		self.t_min_p = self.len_t - self.len_p
 
 		self.power = 10**9
-		self.mod_1 = self.power + 1957
-		self.mod_2 = self.power + 2233
+		self.mod_a = self.power + 1957
+		self.mod_b = self.power + 2233
 		self.x = random.randint(1, self.power)
 
-		self.hashes_1 = [0] * self.len_t
-		self.hashes_2 = [0] * self.len_t
-		self.coefs_1 = [1] * self.len_t
-		self.coefs_2 = [1] * self.len_t
+		self.hashes_1a = [0] * self.len_t
+		self.hashes_1b = [0] * self.len_t
+		self.coefs_1a = [1] * self.len_t
+		self.coefs_1b = [1] * self.len_t
+
+		self.hashes_2a = [0] * self.len_p
+		self.hashes_2b = [0] * self.len_p
+		self.coefs_2a = [1] * self.len_p
+		self.coefs_2b = [1] * self.len_p
+
+		self.precompute_values()
 
 	def precompute_values(self):
 		for i in range(1, self.len_t):
-			self.hashes_1 = (self.x * self.hashes_1[i-1] + ord(self.text[i-1])) % self.mod_1
-			self.hashes_2 = (self.x * self.hashes_2[i-1] + ord(self.text[i-1])) % self.mod_2
+			self.hashes_1a = (self.x * self.hashes_1a[i-1] + ord(self.text[i-1])) % self.mod_a
+			self.hashes_1b = (self.x * self.hashes_1b[i-1] + ord(self.text[i-1])) % self.mod_b
 
-			self.coefs_1 = (self.coefs_1[i-1] * self.x) % self.mod_1
-			self.coefs_2 = (self.coefs_2[i-1] * self.x) % self.mod_2
+			self.coefs_1a = (self.coefs_1a[i-1] * self.x) % self.mod_a
+			self.coefs_1b = (self.coefs_1b[i-1] * self.x) % self.mod_b
+
+		for i in range(1, self.len_p):
+			self.hashes_2a = (self.x * self.hashes_2a[i-1] + ord(self.pattern[i-1])) % self.mod_a
+			self.hashes_2b = (self.x * self.hashes_2b[i-1] + ord(self.pattern[i-1])) % self.mod_b
+
+			self.coefs_2a = (self.coefs_2a[i-1] * self.x) % self.mod_a
+			self.coefs_2b = (self.coefs_2b[i-1] * self.x) % self.mod_b
+
+	def binary_mismatch_search(self, left, right):
+		mismatches = 0
+
+
 
 
 
