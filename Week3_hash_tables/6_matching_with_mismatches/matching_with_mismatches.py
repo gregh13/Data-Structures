f766e669bc4b0414c9671f115d3f210e92e3a2bd
Greg <gregh13@protonmail.com>
2022-10-10 15:10:59 +0700

    complete naive solution

--- Week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
+++ Week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
@@ -2,17 +2,28 @@ import sys
 
 
 def solve(k, text, pattern):
+	results = []
+
 	len_t = len(text)
 	len_p = len(pattern)
+
 	mismatches = 0
+	match_found = False
 
 	for i in range(len_t - len_p):
 		for j in range(len_p):
 			char_t = text[i:(j+1)]
 			if pattern[j] != char_t:
 				mismatches += 1
+				if mismatches > k:
+					match_found = False
+					break
+		if match_found:
+			results.append(i)
+			mismatches = 0
+			match_found = False
 
-	return []
+	return results
 
 
 for line in sys.stdin.readlines():
