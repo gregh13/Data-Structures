d34db8ac7a40db8f47de5009cca73a0a3a6bd3ee
Greg <gregh13@protonmail.com>
2022-10-11 16:43:30 +0700

    reformat starter code, fix initialization of class attributes

--- week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -6,46 +6,45 @@ threading.stack_size(2**27)  # new thread will get stack of such size
 
 
 class TreeOrders:
+    def __init__(self):
+        self.result = []
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for i in range(self.n)]
+        self.left = [0 for i in range(self.n)]
+        self.right = [0 for i in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
 
-  def read(self):
-    self.n = int(sys.stdin.readline())
-    self.key = [0 for i in range(self.n)]
-    self.left = [0 for i in range(self.n)]
-    self.right = [0 for i in range(self.n)]
-    for i in range(self.n):
-      [a, b, c] = map(int, sys.stdin.readline().split())
-      self.key[i] = a
-      self.left[i] = b
-      self.right[i] = c
-
-  def inOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
+    def inorder(self):
+        self.result = []
+        # Finish the implementation
+        # You may need to add a new recursive method to do that
                 
-    return self.result
+        return self.result
 
-  def preOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
+    def preorder(self):
+        self.result = []
+        # Finish the implementation
+        # You may need to add a new recursive method to do that
                 
-    return self.result
+        return self.result
 
-  def postOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def postorder(self):
+        self.result = []
+        # Finish the implementation
+        # You may need to add a new recursive method to do that
+
+        return self.result
 
 
 def main():
     tree = TreeOrders()
-    tree.read()
-    print(" ".join(str(x) for x in tree.inOrder()))
-    print(" ".join(str(x) for x in tree.preOrder()))
-    print(" ".join(str(x) for x in tree.postOrder()))
+    print(" ".join(str(x) for x in tree.inorder()))
+    print(" ".join(str(x) for x in tree.preorder()))
+    print(" ".join(str(x) for x in tree.postorder()))
 
 
 threading.Thread(target=main).start()
