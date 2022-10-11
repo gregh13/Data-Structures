b7c78fe01450744ddb0b015f4c1fabb96b0daf69
Greg <gregh13@protonmail.com>
2022-10-11 19:12:38 +0700

    use my challenge two finished code as starter (since this challenge is the same, but more advanced (duplicate values are allowed)

--- week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -1,23 +1,71 @@
 #!/usr/bin/python3
 
-import sys, threading
+import sys
+import threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+
+def is_bst(tree):
+    def check_inorder(key_index):
+        # Check if null child node
+        if key_index == -1:
+            return
+
+        # Store value for use below; node = [node value, left child index, right child index]
+        node = tree[key_index]
+
+        # Traverse inorder, check left child first
+        check_inorder(node[1])
+
+        # Check that tree is inorder (i.e. ascending in value)
+        if node[0] > results[-1]:
+            # Node value in order, add current node value to list
+            results.append(node[0])
+        else:
+            # Found value not in order, change flag
+            correct_tree[0] = False
+            return
+
+        # Check right child
+        check_inorder(node[2])
+
+        return
+
+    # Initialize results, start with negative infinity for first leaf value comparison
+    results = [float("-inf")]
+
+    # Initialize answer flag as list for easier access within recursive function
+    correct_tree = [True]
+
+    # Start search with root index
+    check_inorder(0)
+
+    return correct_tree[0]
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    # Number of nodes
+    nodes = int(sys.stdin.readline().strip())
+
+    # Initialize empty tree
+    tree = []
+
+    # Build tree
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+
+    # Check if tree exists
+    if len(tree) > 0:
+
+        # Check if tree is a valid binary search tree
+        if is_bst(tree):
+            print("CORRECT")
+        else:
+            print("INCORRECT")
+    else:
+        print("CORRECT")
+
 
 threading.Thread(target=main).start()
