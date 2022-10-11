46c7748dba6b2f39fa67e83ac094f36c4304ddb5
Greg <gregh13@protonmail.com>
2022-10-11 18:14:07 +0700

    implement inorder check for binary tree integrity

--- week4_binary_search_trees/2_is_bst/is_bst.py
+++ week4_binary_search_trees/2_is_bst/is_bst.py
@@ -8,7 +8,27 @@ threading.stack_size(2**25)   # new thread will get stack of such size
 
 
 def is_bst(tree):
-    return True
+    def get_inorder(key_index):
+        if key_index == -1:
+            return
+
+        node = tree[key_index]
+        get_inorder(node[1])
+        if node[0] > results[-1]:
+            results.append(node[0])
+        else:
+            okay_tree[0] = False
+            return
+        get_inorder(node[2])
+
+        return
+
+    # Reset results list
+    results = []
+    okay_tree = [True]
+    # Start search with root index
+    get_inorder(0)
+    return okay_tree[0]
 
 
 def main():
