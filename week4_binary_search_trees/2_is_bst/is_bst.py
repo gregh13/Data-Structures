78648910dc82aca4ab432567e20aff3fb5d25442
Greg <gregh13@protonmail.com>
2022-10-11 18:34:06 +0700

    add and update comments

--- week4_binary_search_trees/2_is_bst/is_bst.py
+++ week4_binary_search_trees/2_is_bst/is_bst.py
@@ -9,26 +9,40 @@ threading.stack_size(2**27)   # new thread will get stack of such size
 
 def is_bst(tree):
     def check_inorder(key_index):
+        # Check if null child node
         if key_index == -1:
             return
 
+        # Store value for use below; node = [node value, left child index, right child index]
         node = tree[key_index]
+
+        # Traverse inorder, check left child first
         check_inorder(node[1])
+
+        # Check that tree is inorder (i.e. ascending in value)
         if node[0] > results[-1]:
+            # Node value in order, add current node value to list
             results.append(node[0])
         else:
-            okay_tree[0] = False
+            # Found value not in order, change flag
+            correct_tree[0] = False
             return
+
+        # Check right child
         check_inorder(node[2])
 
         return
 
-    # Reset results list
+    # Initialize results, start with negative infinity for first leaf value comparison
     results = [float("-inf")]
-    okay_tree = [True]
+
+    # Initialize answer flag as list for easier access within recursive function
+    correct_tree = [True]
+
     # Start search with root index
     check_inorder(0)
-    return okay_tree[0]
+
+    return correct_tree[0]
 
 
 def main():
