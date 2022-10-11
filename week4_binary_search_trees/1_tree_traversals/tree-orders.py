f68f38394bfd66cc2a221eaace8978353750d9d5
Greg <gregh13@protonmail.com>
2022-10-11 17:49:13 +0700

    add comments

--- week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -1,17 +1,20 @@
 # python3
 
 import sys, threading
-sys.setrecursionlimit(10**6) # max depth of recursion
-threading.stack_size(2**27)  # new thread will get stack of such size
+sys.setrecursionlimit(10**6)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
 
 
 class TreeOrders:
     def __init__(self):
-        self.results = []
+        # Initialize input variables and results list
         self.n = int(sys.stdin.readline())
         self.key = [0 for i in range(self.n)]
         self.left = [0 for i in range(self.n)]
         self.right = [0 for i in range(self.n)]
+        self.results = []
+
+        # Read input into appropriate arrays (i.e. build the binary tree input)
         for i in range(self.n):
             [a, b, c] = map(int, sys.stdin.readline().split())
             self.key[i] = a
@@ -20,15 +23,18 @@ class TreeOrders:
 
     def inorder(self):
         def get_inorder(key_index):
+            # Check if null child node
             if key_index == -1:
                 return
 
+            # Traverse to left child, then parent, then right child
             get_inorder(self.left[key_index])
             self.results.append(self.key[key_index])
             get_inorder(self.right[key_index])
 
             return
 
+        # Reset results list
         self.results = []
         # Start search with root index
         get_inorder(0)
@@ -37,15 +43,18 @@ class TreeOrders:
 
     def preorder(self):
         def get_preorder(key_index):
+            # Check if null child node
             if key_index == -1:
                 return
 
+            # Traverse to parent, then left child, then right child
             self.results.append(self.key[key_index])
             get_preorder(self.left[key_index])
             get_preorder(self.right[key_index])
 
             return
 
+        # Reset results list
         self.results = []
         # Start search with root index
         get_preorder(0)
@@ -53,15 +62,18 @@ class TreeOrders:
 
     def postorder(self):
         def get_postorder(key_index):
+            # Check if null child node
             if key_index == -1:
                 return
 
+            # Traverse to right child, then left child, then to parent
             get_postorder(self.left[key_index])
             get_postorder(self.right[key_index])
             self.results.append(self.key[key_index])
 
             return
 
+        # Reset results list
         self.results = []
         # Start search with root index
         get_postorder(0)
