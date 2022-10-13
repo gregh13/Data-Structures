9f6a26c6ce98018119ddf225ff4f6960755f48f9
Greg <gregh13@protonmail.com>
2022-10-13 19:22:02 +0700

    clean up code, add comments

--- week4_binary_search_trees/5_rope/rope.py
+++ week4_binary_search_trees/5_rope/rope.py
@@ -1,5 +1,3 @@
-# python3
-
 import sys
 
 
@@ -11,22 +9,32 @@ class Rope:
 		return self.s
 
 	def process(self, i, j, k):
+
 		# Use slicing to quickly cut and paste parts of a string
 		left_part = self.s[:i]
 		cut_part = self.s[i:j+1]
 		right_part = self.s[j+1:]
 		small_join = left_part + right_part
+
 		# k is the index number of the smaller join list where cut part needs to be inserted
 		new_left = small_join[:k]
 		new_right = small_join[k:]
+
 		# Put everything back together and overwrite original string variable s
 		self.s = new_left + cut_part + new_right
 		return
 
 
+# Read in string
 rope = Rope(sys.stdin.readline().strip())
+
+# Number of cut and paste operations
 q = int(sys.stdin.readline())
+
+# Loop through each operation
 for _ in range(q):
 	i, j, k = map(int, sys.stdin.readline().strip().split())
 	rope.process(i, j, k)
+
+# Print final result
 print(rope.result())
