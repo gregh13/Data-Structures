2d54b0c31cdb2a4fb45311959202e948db220d48
Greg <gregh13@protonmail.com>
2022-10-13 19:05:07 +0700

    implement naive process algorithm

--- week4_binary_search_trees/5_rope/rope.py
+++ week4_binary_search_trees/5_rope/rope.py
@@ -10,8 +10,14 @@ class Rope:
 	def result(self):
 		return self.s
 
-	def process(self, i, j, k):
-		# Write your code here
+	def process_naive(self, i, j, k):
+		left_part = self.s[:i]
+		cut_part = self.s[i:j]
+		right_part = self.s[j:]
+		small_join = left_part + right_part
+		new_left = small_join[:k]
+		new_right = small_join[k:]
+		self.s = new_left + cut_part + new_right
 		pass
 
 
@@ -19,5 +25,5 @@ rope = Rope(sys.stdin.readline().strip())
 q = int(sys.stdin.readline())
 for _ in range(q):
 	i, j, k = map(int, sys.stdin.readline().strip().split())
-	rope.process(i, j, k)
+	rope.process_naive(i, j, k)
 print(rope.result())
